efe09d9f59cf5eb6db13ef71313026230bb9d94b
iamceph <frantisek@nvsd.dev>
2022-03-17 11:45:34 +0100

    Oops

--- gradle-plugin/src/main/groovy/com/iamceph/springed/rsocket/SpringedRSocketExtension.groovy
+++ gradle-plugin/src/main/groovy/com/iamceph/springed/rsocket/SpringedRSocketExtension.groovy
@@ -3,6 +3,6 @@ package com.iamceph.springed.rsocket
 class SpringedRSocketExtension {
     String rsocketVersion = "1.1.1"
     String rsocketRpcVersion = "0.3.0"
-    String springedRsocketVersion = "0.1.0-SNAPSHOT"
+    String springedRsocketVersion = "1.0.1"
     String protobufVersion = "3.19.2"
 }
